7af630f9dfe86045da9f5ac70db7adad784f3159
kimmeh <mhko0411@gmail.com>
2022-06-20 22:50:04 +0900

    login form에 react hook form 적용

--- src/components/authentication/login/index.tsx
+++ src/components/authentication/login/index.tsx
@@ -1,11 +1,22 @@
 import type { FC } from "react";
 import { useState, useEffect } from "react";
-import Box from "@mui/material/Box";
+import { useForm } from "react-hook-form";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 
+const INPUT_OPTION = {
+  id: {
+    required: true,
+  },
+  password: {
+    required: true,
+  },
+};
+
 const Login: FC = () => {
+  const { register, handleSubmit } = useForm();
+
   return (
     <>
       <Typography variant="h4">로그인</Typography>
@@ -16,6 +27,7 @@ const Login: FC = () => {
           width: "450px",
           height: "60px",
         }}
+        {...register("id", INPUT_OPTION.id)}
       />
       <TextField
         type="password"
@@ -24,6 +36,7 @@ const Login: FC = () => {
           width: "450px",
           height: "60px",
         }}
+        {...register("password", INPUT_OPTION.password)}
       />
       <Button
         variant="contained"
